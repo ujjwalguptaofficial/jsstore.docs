3c1f3601b6f8a7fe0aa4c14eb43ab3af58a12148
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-05-10 23:12:07 +0530

    change getstarted url in index page

--- src/codes/code/pages/index.vue
+++ src/codes/code/pages/index.vue
@@ -16,7 +16,7 @@
                         <br>JsStore is an IndexedDB Wrapper. It makes IndexedDB super easy with its SQL like apis.
                     </div>
                     <div id="divLinkGroup">
-                        <v-btn href="tutorial/get_started">Get Started</v-btn>
+                        <v-btn href="tutorial/getstarted">Get Started</v-btn>
                         <v-btn href="https://github.com/ujjwalguptaofficial/JsStore" target="_blank">Download v2.0.0</v-btn>
                     </div>
                     <div class="margin-top-50px top-border">
