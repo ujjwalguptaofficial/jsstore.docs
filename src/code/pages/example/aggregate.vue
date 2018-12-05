be549a954d589d9e8d40e766a563c250f4df0cac
Travis CI <ujjwalkumargupta44@gmail.com>
2018-12-05 12:33:41 +0000

    Deploy to GitHub Pages: a798e1ff0c5c4c48d76129861ae4639c64890630

--- src/code/pages/example/aggregate.vue
+++ src/code/pages/example/aggregate.vue
@@ -10,7 +10,7 @@ import IdbStudioAppender from "../../components/idbstudio_appender.vue";
   }
 })
 export default class extends Vue {
-  exampleContent = `%3Cpre%3E%3Ccode%3Eselect(%7B%0A%20%20%20%20from:%20%22Products%22,%0A%20%20%20%20aggregate:%20%7B%0A%20%20%20%20%20%20%20%20min:%20%22price%22%20//%20column%20name%0A%20%20%20%20%7D%0A%7D);%0A%3C/code%3E%3C/pre%3E`;
+  exampleContent = `%3Cpre%3E%3Ccode%3Eselect(%7B%0A%20%20%20%20from:%20%22Products%22,%0A%20%20%20%20aggregate:%20%7B%0A%20%20%20%20%20%20%20%20min:%20%22price%22%0A%7D);%0A%3C/code%3E%3C/pre%3E`;
   title="Aggregate";
 }
 </script>
