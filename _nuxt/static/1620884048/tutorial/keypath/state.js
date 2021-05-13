9a91ab903714c475d6220ae5a4dd269083d7735a
Travis CI <ujjwalkumargupta44@gmail.com>
2021-05-13 05:34:09 +0000

    Deploy to GitHub Pages: 3484539b25161248ccf2aefe1abea9d5849b8534

---
+++ _nuxt/static/1620884048/tutorial/keypath/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620884048",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorial\u002Fkeypath",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
