c807a18c415f6feca25fb1ba9ca0a18d308d2bd6
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-05-22 16:30:39 +0530

    add tab support

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -8,12 +8,13 @@
 
 <script>
 import Vue from "vue";
-import "../styles/index.scss";
+import "@/styles/index.scss";
 import Menu from "@/components/menu.vue";
 import AppFooter from "@/components/footer.vue";
 Vue.filter("imgPath", (val) => {
   return "/img/" + val;
 });
+
 export default {
   components: { Menu, AppFooter },
   mounted() {
@@ -25,6 +26,54 @@ export default {
       link.href = "#" + el.id;
       link.setAttribute("aria-hidden", "true");
       el.appendChild(link);
+    });
+    const tabsElement = document.querySelector("div.tabs");
+    if (tabsElement == null) return;
+    let activeIndex;
+    const tabs = tabsElement.querySelectorAll(".tab");
+    const tabNameItems = [];
+
+    const tabNames = document.createElement("div");
+    tabNames.className = "tabs__names";
+    tabsElement.appendChild(tabNames);
+
+    tabs.forEach((item, index) => {
+      const name = item.getAttribute("name");
+      const tab = document.createElement("div");
+      tab.innerText = name;
+      tab.className = "tabs__names__item";
+      tab.setAttribute("target", name);
+      item.style.cssText = "display:none";
+      if (item.getAttribute("active") == "true") {
+        activeIndex = index;
+      }
+      tabNameItems.push(tab);
+    });
+    const tabContent = document.createElement("div");
+    tabContent.className = "tabs__content";
+    tabsElement.appendChild(tabContent);
+    tabContent.innerHTML = "OKKKKKK";
+
+    activeIndex = activeIndex || 0;
+
+    tabNameItems.forEach((tab, index) => {
+      tabNames.appendChild(tab);
+      tab.onclick = (e) => {
+        const el = e.target;
+        tabNameItems[activeIndex].classList.remove("active");
+
+        activeIndex = index;
+
+        el.classList.add("active");
+        const target = el.getAttribute("target");
+        const targetTab = tabsElement.querySelectorAll(".tab")[activeIndex];
+        if (targetTab) {
+          tabContent.innerHTML = targetTab.innerHTML;
+        }
+      };
+      if (index === activeIndex) {
+        tab.click();
+      }
     });
   },
 };
