af99fb8d1b19c99e7542d5420b52c95adb0ac28b
Travis CI <ujjwalkumargupta44@gmail.com>
2021-06-13 14:18:36 +0000

    Deploy to GitHub Pages: 6d9a0295521ac95d25960539a717b0dc57f86c72

---
+++ _nuxt/static/1623593914/tutorial/v3-to-v4/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623593914",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorial\u002Fv3-to-v4",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
