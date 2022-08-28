927835914e6d68d7cdfd36910659c4867266d4d2
ujjwalguptaofficial <ujjwalkumargupta4u@gmail.com>
2022-08-28 02:36:31 +0000

    Deploying to gh-pages from @ ujjwalguptaofficial/jsstore.docs@b534ccecf30ef252d87c934e47490b855ddc7afd 🚀

---
+++ _nuxt/static/1661654188/tutorial/select/skip/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661654188",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorial\u002Fselect\u002Fskip",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
