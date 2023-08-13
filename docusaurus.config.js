9ed06f9c4c199a51b55a0f72814b4fc0018cefe8
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-08-13 09:09:45 +0400

    update favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'JsStore',
   tagline: 'Think in SQL and do in javascript',
-  favicon: 'img/favicon.ico',
+  favicon: 'favicon.ico',
 
   // Set the production url of your site here
   url: 'https://jsstore.net',
