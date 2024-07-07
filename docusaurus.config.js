2599498d6ba8be92e4a29e43769c7838ef15be1a
ujjwalgupta94 <ujjwal@matic.network>
2024-07-07 14:07:20 +0530

    move trcking script to headTags

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -178,12 +178,17 @@ const config = {
     {
       src: 'https://www.googletagmanager.com/gtag/js?id=AW-940326991',
       async: true,
-    },
-    `
+    }
+  ],
+  headTags:[
+    {
+      tagName:'script',
+      innerHTML: `
       window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-940326991');
       <!-- Event snippet for Page view conversion page -->  
       gtag('event', 'conversion', {'send_to': 'AW-940326991/9xLtCMLc0MAZEM-AscAD'});
       `
+    }
   ]
 };
 
